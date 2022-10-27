82606e1aeeb80d133334421b36542788a0d4f99e
timsonner <66705347+timsonner@users.noreply.github.com>
2022-10-26 22:32:29 -0600

    Initial Commit

---
+++ Realm of Magic/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Realm of Magic
+//
+//  Created by Timothy Sonner on 10/26/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
